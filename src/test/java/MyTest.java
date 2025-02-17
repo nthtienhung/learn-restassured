75b4b033282bfbc8886a6016e557d44acbfe46ba
Hung Tien <hungnt2@its.local>
2025-02-17 18:19:26 +0700

    asd

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -7,7 +7,10 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.*;
+
+import org.hamcrest.Matchers;
 
 public class MyTest {
 
@@ -20,8 +23,13 @@ public class MyTest {
     @Test
     void getReq(){
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-        RestAssured.basePath = "posts";
-        given().when().get().then().log().all().statusCode(200).body("size()", greaterThan(0));
+        // RestAssured.basePath = "posts";
+        given().
+            when().
+                get("posts/2").
+        then().
+            statusCode(200).
+            body("", Matchers.hasSize(3));
     }
 
     @AfterSuite
